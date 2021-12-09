0e4a117bca4920c313408163b80563cfc0e42b2e
miwume <95006778+miwume@users.noreply.github.com>
2021-12-09 12:25:03 +0900

    Update script.js

--- what-is-js/script.js
+++ what-is-js/script.js
@@ -1,6 +1,6 @@
 function createParagraph() {
   const para = document.createElement('p');
-  para.textContent = '🍬';
+  para.textContent = 'ぴぃ！ぴーぃっ！びかぢゅゅうー！ぴかぁ！ぴがぴか！ぴかぴかっ！ぴかちゅう！';
   document.body.appendChild(para);
 }
 
